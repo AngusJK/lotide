acfe18ff60a2a6cc704ed194a75c3ebda3e71edf
Angus Kellett <anguskellett@hotmail.com>
2020-10-06 19:53:27 +0000

    Corrects to allow for string iteration

--- takeUntil.js
+++ takeUntil.js
@@ -23,28 +23,30 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`🔴 Assertion failed: ${actual} !== ${expected}`);
   }
 }; 
-const temperatures = [-25, -19, -8, 6, 4, 0, -7];
+const temperatures = [25, 19, -8, 6, 4, 0, -7];
 const temperatures2 = [-4, -5, -28];
 const temperatures3 = [34, -3, -54];
+const letters = ["a", "b", "c", "d", "e"];
 
 const takeUntil = function(array, callback) {
-  let belowZero = [];
+  let filtered = [];
   for (let number of array ) {
-    if (callback(number)) {
-      belowZero.push(number);
+    if (!callback(number)) {
+      filtered.push(number);
     } else {
-      return belowZero
+      return filtered;
     }
   }
-  return belowZero;
+  return filtered;
 }
 
-const freezing = takeUntil(temperatures, temperature => temperature < 0);
-const stillFreezing = takeUntil(temperatures2, temperature => temperature < 0);
-const notSoFreezing = takeUntil(temperatures3, temperature => temperature < 0);
+const aboveZero = takeUntil(temperatures, temperature => temperature < 0);
+const aboveZero2 = takeUntil(temperatures2, temperature => temperature < 0);
+const aboveZero3 = takeUntil(temperatures3, temperature => temperature < 0);
+const upToD = takeUntil(letters, letter => letter === "d");
 
-assertArraysEqual(freezing, [-25, -19, -8]);
-assertArraysEqual(stillFreezing, [-4, -5, -28]);
-assertArraysEqual(notSoFreezing, []);
-
+assertArraysEqual(aboveZero, [25, 19]);
+assertArraysEqual(aboveZero2, []);
+assertArraysEqual(aboveZero3, [34]);
+assertArraysEqual(upToD, ["a", "b", "c"]);
 
