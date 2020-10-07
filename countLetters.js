8616b5e77d54e5fca6555bfa087f65bc591f5b07
Angus Kellett <anguskellett@hotmail.com>
2020-10-07 00:33:08 +0000

    returns the number of instances of each letter in a given string

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,42 @@
+function eqArrays(firstArray, secondArray) {
+  let inconsistencies = 0;
+  let status = true;
+  if (firstArray.length === secondArray.length) {
+    for (let i = 0; i < firstArray.length; i++) {
+      if (firstArray[i] !== secondArray[i]) {
+        inconsistencies += 1;
+      }
+   }
+  } else {
+    inconsistencies += 1;
+  }
+  if (inconsistencies > 0) {
+    status = false;
+  }
+  return status;
+}
+
+const eqObjects = function(object1, object2) {
+  let result = true;
+  let object1Keys = Object.keys(object1);
+  let object2Keys = Object.keys(object2);
+  if (object1Keys.length !== object2Keys.length) {
+    result = false;
+  }
+  for (key of object1Keys) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (eqArrays(object1[key], object2[key]) === false) {
+        result = false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      result = false;
+    }
+  }
+  return result;
+}
 const assertEqual = function(actual, expected) {
-  if (JSON.stringify(actual) === JSON.stringify(expected)) {
+  if (eqObjects(actual) === eqObjects(expected)) {
+  // if (JSON.stringify(actual) === JSON.stringify(expected)) {
     console.log(`✅ Assertion passed: ${actual} === ${expected}`);
   } else {
     console.log(`🔴 Assertion failed: ${actual} !== ${expected}`);
@@ -30,4 +67,5 @@ const countLetters = function(word) {
 assertEqual(countLetters("trout"), { t: 2, r: 1, o: 1, u: 1 });
 assertEqual(countLetters("lotide"), { l: 1, o: 1, t: 1, i: 1, d: 1, e: 1 });
 assertEqual(countLetters("a bb ccc dddd"), { a: 1, b: 2, c: 3, d: 4 });
+
 
