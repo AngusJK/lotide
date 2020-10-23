0292a41a898b69f3010d2ed8c0e9037726fff038
Angus Kellett <anguskellett@hotmail.com>
2020-10-23 00:30:06 +0000

    Implements recursive function to test whether nested objects are equal

--- eqObjects.js
+++ eqObjects.js
@@ -25,23 +25,44 @@ function eqArrays(firstArray, secondArray) {
 }
 
 const eqObjects = function(object1, object2) {
-  let result = true;
+  let passes = true;
   let object1Keys = Object.keys(object1);
   let object2Keys = Object.keys(object2);
   if (object1Keys.length !== object2Keys.length) {
-    result = false;
-  }
-  for (key of object1Keys) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (eqArrays(object1[key], object2[key]) === false) {
-        result = false;
-      }
-    } else if (object1[key] !== object2[key]) {
-      result = false;
+    passes = false;
+  } else {
+    for (let key of object1Keys) {
+      if (typeof object1[key] === 'object' && typeof object2[key] === 'object') { 
+        if (Array.isArray(object1[key]) === false && Array.isArray(object2[key]) === false) {
+          if (eqObjects(object1[key], object2[key]) === false) {
+            passes = false;
+          };
+        } else if (Array.isArray(object1[key]) === true && Array.isArray(object2[key]) === true) {
+          if (eqArrays(object1[key], object2[key]) === false) {
+            passes = false;
+          }
+        } else {
+          passes = false;
+        }
+      } else if (object1[key] !== object2[key]) {
+         passes = false;
+       }
     }
   }
-  return result;
+  return passes;
 }
+
+console.log(eqObjects({ a: 1, b: 2 }, { b: 2, a: 1 })); // => true
+console.log(eqObjects({ a: 1, b: 2 }, { c: 2, a: 1 })); // => false
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => true
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })) // => false
+console.log(eqObjects({ a: ['b, c'], b: 3}, { a: ['e, f'], b: 3 })); // => false
+console.log(eqObjects({ a: { b: 'a' }, c: 1}, { a: ['a'], c: 1})); // => false
+console.log(eqObjects({ a: 1, b: 2, c: 3 }, { a: 1, b: 2, c: 3 } )); // => true
+console.log(eqObjects({ a: [3, 4, 5], b: [7, 8, 9] }, { a: [3, 4, 5], b: [7, 8, 9] })); // => true
+console.log(eqObjects({ a: { b: 1, c: 2, d: { e: 3, f: 4 }}, g: 5 }, { a: { b: 1, c: 2, d: { e: 3, f: 7 }}, g: 5 })); // => false
+console.log(eqObjects({ a: { b: 1, c: 2, d: { e: 3, f: 4}}, g: 5}, {a: { b: 1, c: 2, d: { e: 3, f: 4}}, g: 5})); // => true
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
