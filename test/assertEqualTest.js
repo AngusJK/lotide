316eea6acc261f4e8963ba0b16b4208e22db4e0f
Angus Kellett <anguskellett@hotmail.com>
2020-10-30 00:19:58 +0000

    adds headTest and assertEqualTest files

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Joe Biden", "Joe Biden");
+assertEqual("George Bush", "Bill Clinton");
+assertEqual(true, true);
+assertEqual(46, 45);
