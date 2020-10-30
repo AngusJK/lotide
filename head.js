cccd001561469a1541d74052e73e86e3640c18c9
Angus Kellett <anguskellett@hotmail.com>
2020-10-30 00:19:03 +0000

    removes test code and adds module.exports and requires assertEqual

--- head.js
+++ head.js
@@ -1,18 +1,9 @@
-function head(array) {
+const assertEqual = require('./assertEqual');
+
+const head = function(array) {
   let value = array[0];
   return value
-}
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 Assertion failed: ${actual} !== ${expected}`);
-  }
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([9]), 9);
-assertEqual(head([]), undefined);
+module.exports = head;
 
