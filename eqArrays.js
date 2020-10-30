4f0a7bbeb755ae1e278231c3071ce23988b5837d
Angus Kellett <anguskellett@hotmail.com>
2020-10-30 01:02:57 +0000

    removes tests, requires assertEqual, adds function export

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 function eqArrays(firstArray, secondArray) {
   let inconsistencies = 0;
@@ -28,13 +22,4 @@ function eqArrays(firstArray, secondArray) {
   return status;
 }
 
-console.log(eqArrays( [[2, 3], [4]], [[2, 3], [4]] )); // => true
-console.log(eqArrays( [[2, 3], [4]], [[2, 3], [4, 5]] )); // => false
-console.log(eqArrays( [[2, 3], [4]], [[2, 3], 4] )); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 4, 3]), false);
-assertEqual(eqArrays(["a", "b", "c"], ["a", "b", "c"]), true);
-assertEqual(eqArrays(["a", "b", "c"], ["a", "b", "C"]), false);
-assertEqual(eqArrays(["a", "b", "c"], ["a", "b"]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
