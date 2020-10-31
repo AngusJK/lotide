0c0815fa93fa17081fec809ffd38237eacb0a5c3
Angus Kellett <anguskellett@hotmail.com>
2020-10-31 03:33:54 +0000

    incorporates mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,4 +1,3 @@
-//const assertEqual = require("../assertEqual");
 const assert = require('chai').assert;
 const head   = require('../head');
 
@@ -7,7 +6,7 @@ describe("#head", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
   it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '5'); 
+    assert.strictEqual(head(['5']), '5');
   });
   it("returns 5 when passed [5,6,7]", () => {
     assert.strictEqual(head([5, 6, 7]), 5);
@@ -20,5 +19,5 @@ describe("#head", () => {
   });
   it("returns undefined when passed an empty array", () => {
     assert.strictEqual(head([]), undefined);
-  } );
+  });
 });
