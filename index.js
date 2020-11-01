c1153e647af1729bc87d0af01e67a45b903b827b
Angus Kellett <anguskellett@hotmail.com>
2020-11-01 05:02:10 +0000

    uses new object syntax and comments out certain functions

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
-const assertObjectsEqual = require('./assertObjectsEqual');
-const countLetters = require('./countLetters');
-const countOnly = require('./countOnly');
+//const assertObjectsEqual = require('./assertObjectsEqual');
+//const countLetters = require('./countLetters');
+//const countOnly = require('./countOnly');
 const eqArrays = require('./eqArrays');
-const eqObjects = require('./eqObjects');
-const findKey = rquire('./findKey');
-const findKeyByValue = require('./findKeyByValue');
-const flatten = require('./flatten');
+//const eqObjects = require('./eqObjects');
+//const findKey = rquire('./findKey');
+//const findKeyByValue = require('./findKeyByValue');
+//const flatten = require('./flatten');
 const head   = require('./head');
-const letterPositions = require('./letterPositions');
-const check = require('./luhn_algorithm');
-const main = require('./main');
-const map = require('./map');
-const maxProfid = require('./maxProfit');
+//const letterPositions = require('./letterPositions');
+//const check = require('./luhn_algorithm');
+//const main = require('./main');
+//const map = require('./map');
+//const maxProfid = require('./maxProfit');
 const middle = require('./middle');
-const sayHelloTo = require('./moduleCheck');
-const poppin_bottles = require('./poppin_bottles');
+//const sayHelloTo = require('./moduleCheck');
+//const poppin_bottles = require('./poppin_bottles');
 const tail   = require('./tail');
-const takeUntil = require('./takeUntil');
-const without = require('./without');
+//const takeUntil = require('./takeUntil');
+//const without = require('./without');
 
 module.exports = {
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  head:   head,
-  letterPositions: letterPositions,
-  check: check,
-  main: main,
-  map: map,
-  middle: middle,
-  sayHelloTo: sayHelloTo,
-  poppin_bottles: poppin_bottles,
-  tail:   tail,
-  takeUntil: takeUntil,
-  without: without
+  assertArraysEqual,
+  assertEqual,
+  //assertObjectsEqual: assertObjectsEqual,
+  //countLetters: countLetters,
+  //countOnly: countOnly,
+  eqArrays,
+  //eqObjects: eqObjects,
+  //findKey: findKey,
+  //findKeyByValue: findKeyByValue,
+  //flatten: flatten,
+  head,
+  //letterPositions: letterPositions,
+  //check: check,
+  //main: main,
+  //map: map,
+  middle,
+  //sayHelloTo: sayHelloTo,
+  //poppin_bottles: poppin_bottles,
+  tail,
+  //takeUntil: takeUntil,
+  //without: without
 };
