1410201965d0df240f67453d7cd7d46d0dc91dbc
Angus Kellett <anguskellett@hotmail.com>
2020-11-03 20:47:37 +0000

    corrects test cases so they all pass

--- test/findKeyByValueTest.js
+++ test/findKeyByValueTest.js
@@ -1,8 +1,8 @@
 const assert = require('chai').assert;
 const findKeyByValue = require('../findKeyByValue');
 
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
@@ -17,18 +17,18 @@ const bestPlayers = {
 
 describe('#findKeyByValue', () => {
   it("returns <drama> when passed an object containing <drama: 'The Wire'> and the value <'The Wire'>", () => {
-    assert.deepEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
   });
   it("returns <undefined> when passed a value and an object that doesn't contain that value", () => {
-    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"));
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
   });
   it("returns <giants> when passed an object conatining <giants: 'Barry Bonds'> and the value <'Barry Bonds'>", () => {
-    assert.strictEqual(findKeyByValue(bestPlayers, "Barry Bonds"));
+    assert.strictEqual(findKeyByValue(bestPlayers, "Barry Bonds"), "giants");
   });
   it("returns <redsox> when passed an object containing <redsox: 'Ted Williams'> and the value <'Ted Williams'>", () => {
-    assert.strictEqual(findKeyByValue(bestPlayers, "Ted Williams"));
+    assert.strictEqual(findKeyByValue(bestPlayers, "Ted Williams"), "redsox");
   });
   it("returns <undefined> when passed a value and an object that doesn't contain that value", () => {
-    assert.strictEqual(findKeyByValue(bestPlayers, "Ernie Banks"));
+    assert.strictEqual(findKeyByValue(bestPlayers, "Ernie Banks"), undefined);
   });
 });
