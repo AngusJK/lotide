a069daf96b1b07debe2e978f6bf42a2dc8d1b38f
Angus Kellett <anguskellett@hotmail.com>
2020-11-03 05:35:20 +0000

    cleans up with eslint, adds module.exports, removes tests

--- countOnly.js
+++ countOnly.js
@@ -1,14 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
 const countOnly = function(allItems, itemsToCount) {
   let filtered = {};
-  for (var name in itemsToCount) {
+  for (let name in itemsToCount) {
     if (itemsToCount[name]) {
       let count = 0;
       for (let i = 0; i < allItems.length; i++) {
@@ -16,30 +8,12 @@ const countOnly = function(allItems, itemsToCount) {
           count += 1;
         }
       }
-      if (count > 0 ) {
+      if (count > 0) {
         filtered[name] = count;
-      }  
+      }
     }
   }
   return filtered;
-}
+};
 
-// Test cases
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined); 
+module.exports = countOnly;
