b19e63ebbf0a031898702ac6f0afd72403083f39
Angus Kellett <anguskellett@hotmail.com>
2020-11-04 23:15:51 +0000

    removes tests, adds module.exports, eslint

--- maxProfit.js
+++ maxProfit.js
@@ -1,6 +1,6 @@
 const maxProfit = function(input) {
   let differences = [];
-  for (let i = 0; i < input.length - 1; i++ ) {
+  for (let i = 0; i < input.length - 1; i++) {
     for (let j = i + 1; j < input.length; j++) {
       differences.push(input[i] - input[j]);
     }
@@ -11,16 +11,9 @@ const maxProfit = function(input) {
       largest = differences[i];
     }
   }
-  console.log(largest);  
-}
+  return largest;
+};
 
-const applePrices = [4, 6, 2, 69, 48, 33, 25];
-const amazonPrices = [54, 39, 48, 19, 21, 17];
-const microsoftPrices = [15, 17, 17];
-const exxonPrices = [14, 29, 33, 48, 56, 68, 71];
+module.exports = maxProfit;
 
-maxProfit(applePrices); // => 44
-maxProfit(amazonPrices); // => 37
-maxProfit(microsoftPrices); // => 0
-maxProfit(exxonPrices); // => -1
 
