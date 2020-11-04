92673f3a00bb7efebdc7630e7e40eb6a14960592
Angus Kellett <anguskellett@hotmail.com>
2020-11-04 23:21:07 +0000

    cleans up formatting with eslint

--- poppin_bottles.js
+++ poppin_bottles.js
@@ -1,7 +1,7 @@
 const args = process.argv.slice(2);
 
 const poppinBottles = function(dollars) {
-  let bottleTotal = Math.trunc(dollars / 2); 
+  let bottleTotal = Math.trunc(dollars / 2);
   let capTotal = bottleTotal;
   let counter = bottleTotal;
   let bottleTrades = 0;
@@ -16,8 +16,8 @@ const poppinBottles = function(dollars) {
     }
     bottleTotal += 1;
     capTotal += 1;
-    counter += 1
-  }
+    counter += 1;
+  };
   while (bottleTotal >= 2 || capTotal >= 4) {
     if (bottleTotal >= 2) {
       tradeIn("bottles");
@@ -26,10 +26,10 @@ const poppinBottles = function(dollars) {
       tradeIn("caps");
     }
   }
-  console.log(`TOTAL BOTTLES: ${counter}`)
+  console.log(`TOTAL BOTTLES: ${counter}`);
   console.log(`REMAINING BOTTLES: ${bottleTotal}`);
   console.log(`REMAINING CAPS: ${capTotal}`);
   console.log(`TOTAL EARNED: \n BOTTLES: ${bottleTrades} \n CAPS: ${capTrades}`);
-}
+};
 
 poppinBottles(args);
